c145d33c9a84c9923afe8855f17056b33c68bdbf
phantom388 <phyowaikyaw@gmail.com>
2013-04-30 18:31:33 -0700

    instance3

--- instance3.py
+++ instance3.py
@@ -19,16 +19,13 @@ def instance3():
         L = nx.disjoint_union(L,item)
 
     L.add_node(99)
-
-    for i in lst:
-        L.add_edge(99,i.nodes()[0]) 
-    
+          
     """
     L.add_node(99)
     L.add_edges_from([(0,99),(9,99),(18,99),(27,99),(36,99),(45,99),(54,99),(63,99),(72,99),(81,99),(90,99)])
+    """
     for i in range(1500):
         x = int(random()*100)
         y = int(random()*100)
         L.add_edge(x,y)
-        """
     return L
