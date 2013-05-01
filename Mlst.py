da8281a5f81c099c5009f4a7016acab424b8b1da
zayd <zayd.enam@gmail.com>
2013-04-30 21:20:37 -0700

    Made one_edge_swap easy

--- Mlst.py
+++ Mlst.py
@@ -55,7 +55,16 @@ def one_edge_swap(G):
     Output: Generate a T one edge swap output
     """
    
-    T = degBasedMST(G);
+    T1 = degBasedMST(G);
+    T2 = unityMST(G);
+
+    print "Degree Based MST: " + str(list(T1.degree(T1.nodes()).values()).count(1)) + " Leaves"
+    print "Unity MST: " + str(list(T2.degree(T2.nodes()).values()).count(1)) +  " Leaves"
+
+    if list(T1.degree(T1.nodes()).values()).count(1) > list(T2.degree(T2.nodes()).values()).count(1):
+        T = T1.copy()
+    else: 
+        T = T2.copy()
 
     for e in list(set(G.edges()).difference(set(T.edges()))):
         U = T.copy()
@@ -70,14 +79,12 @@ def one_edge_swap(G):
 
             newDegrees = list(U.degree(U.nodes()).values()) 
 
-            print newDegrees.count(1) 
-            print Degrees.count(1)
-
             if newDegrees.count(1) > Degrees.count(1): 
-                print "Changing"
                 T = U.copy()
 
             U.add_edge(f[0],f[1]);
+
+    print "One Edge Swap: " + str(list(T.degree(T.nodes()).values()).count(1)) + " Leaves"
 
     return T
 
