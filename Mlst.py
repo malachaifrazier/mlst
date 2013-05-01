d108e6a3dd4a88d61f36ed33c16c63383dd326c2
phantom388 <phyowaikyaw@gmail.com>
2013-04-30 17:17:45 -0700

    updated Mlst

--- Mlst.py
+++ Mlst.py
@@ -21,13 +21,169 @@ def setEdgeWeights(G):
 
 # <codecell>
 
-def deg_based_MST(G):
+def unityEdgeWeights(G):
+    """
+    Input: Unweighted networkx undirected graph G
+    Output: Weighted networkx undirected graph where weight of edge is one
+    """
+    for e in G.edges():
+        G.edge[e[0]][e[1]]['weight'] = 1
+
+# <codecell>
+
+def degBasedMST(G):
     """
     Input: Unweighted or Weighted networkx undirected graph G (Caution weights will be overwritten)
     Output: MST of G where weight of edge is max degree of its endpoint vertexes
     """
     setEdgeWeights(G)
-    return MinimumSpanningTree(G)
+    mstEdgeLst = mst.MinimumSpanningTree(G)
+    T = nx.Graph()
+    T.add_edges_from(mstEdgeLst)
+    return T
+
+# <codecell>
+
+def unityMST(G):
+    """
+    Input: Unweighted or Weighted networkx undirected graph G (Caution weights will be overwritten)
+    Output: MST of G where all edge weights are one
+    """
+    unityEdgeWeights(G)
+    mstEdgeLst = mst.MinimumSpanningTree(G)
+    T = nx.Graph()
+    T.add_edges_from(mstEdgeLst)
+    return T
+
+# <codecell>
+
+G1 = nx.wheel_graph(5)
+nx.draw(G1)
+
+# <codecell>
+
+nx.draw(unityMST(G1))
+
+# <codecell>
+
+nx.draw(degBasedMST(G1))
+
+# <codecell>
+
+G2 = nx.hypercube_graph(3)
+nx.draw(G2)
+
+# <codecell>
+
+nx.draw(unityMST(G2))
+
+# <codecell>
+
+nx.draw(degBasedMST(G2))
+
+# <codecell>
+
+G3 = nx.barbell_graph(4,2)
+nx.draw(G3)
+
+# <codecell>
+
+nx.draw(unityMST(G3))
+
+# <codecell>
+
+nx.draw(degBasedMST(G3))
+
+# <codecell>
+
+G4 = nx.complete_graph(6)
+nx.draw(G4)
+
+# <codecell>
+
+nx.draw(unityMST(G4))
+
+# <codecell>
+
+nx.draw(degBasedMST(G4))
+
+# <codecell>
+
+G5 = nx.complete_bipartite_graph(3,5)
+nx.draw(G5)
+
+# <codecell>
+
+nx.draw(unityMST(G5))
+
+# <codecell>
+
+nx.draw(degBasedMST(G5))
+
+# <codecell>
+
+G6 = nx.cycle_graph(5)
+nx.draw(G6)
+
+# <codecell>
+
+nx.draw(unityMST(G6))
+
+# <codecell>
+
+nx.draw(degBasedMST(G6))
+
+# <codecell>
+
+G7 = nx.lollipop_graph(5,3)
+nx.draw(G7)
+
+# <codecell>
+
+nx.draw(unityMST(G7))
+
+# <codecell>
+
+nx.draw(degBasedMST(G7))
+
+# <codecell>
+
+G8 = nx.circular_ladder_graph(5)
+nx.draw(G8)
+
+# <codecell>
+
+nx.draw(unityMST(G8))
+
+# <codecell>
+
+nx.draw(degBasedMST(G8))
+
+# <codecell>
+
+G9 = nx.star_graph(5)
+nx.draw(G9)
+
+# <codecell>
+
+nx.draw(unityMST(G9))
+
+# <codecell>
+
+nx.draw(degBasedMST(G9))
+
+# <codecell>
+
+G10 = nx.gnp_random_graph(8,.5)
+nx.draw(G10)
+
+# <codecell>
+
+nx.draw(unityMST(G10))
+
+# <codecell>
+
+nx.draw(degBasedMST(G10))
 
 # <codecell>
 
