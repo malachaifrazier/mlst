5dfa20e26f568bee01559153e7b63b54dae8e5b7
diivanand <diivanand@gmail.com>
2013-05-01 19:42:08 -0700

    latest build

--- script_out.py
+++ script_out.py
@@ -1,9 +1,19 @@
 import networkx as nx
 import out
-graphList = [] 
-#stuff here to populate graphList
-outlist = convert_graphs(graphList)
+import sys
+import Mlst as mlst
+output = mlst.get_three_hardest_instances(int(sys.argv[1]))
+hard_instance_graphs = output[0]
+hard_instance_mlsts = output[1]
+in_stuff = out.convert_graphs(hard_instance_graphs)
+out_stuff = out.convert_graphs(hard_instance_mlsts)
+
+f = open('hard.in', 'w')
+for each in in_stuff:
+	f.write(each)
+f.close()
 
 f = open('hard.out', 'w')
-for each in outlist:
+for each in out_stuff:
 	f.write(each)
+f.close()
