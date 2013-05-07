d3bfd83f57871f8d41cefc659d79cf024ee3fafb
diivanand <diivanand@gmail.com>
2013-05-06 17:27:48 -0700

    output scripts

---
+++ script_out_2.py
@@ -0,0 +1,17 @@
+import input_graph_reader as igr
+import Mlst as mlst
+import sys
+import out
+
+in_file = sys.argv[1]
+
+graph_list = igr.get_nx_graphs(in_file)
+
+our_mlsts = [mlst.one_edge_swap(graph) for graph in graph_list]
+
+out_stuff = out.convert_graphs(our_mlsts)
+
+f = open('hard.all.v3.out', 'w')
+for each in out_stuff:
+	f.write(each)
+f.close()
